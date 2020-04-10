7be659d16aa15fda6c719b63b614f87da6c3ee02
Leo Lin <contemplation8213@gmail.com>
2020-04-10 22:32:32 +0800

    調整上 server 的部署設置

--- src/router.ts
+++ src/router.ts
@@ -5,9 +5,12 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/',
       name: 'home',
